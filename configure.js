a2c18182a64e96ace49e04c46ee196f36552b1c6
Marcin Konarski <marcin.konarski@codestation.org>
2019-07-22 19:39:31 +0200

    configure.js, cmake: Allow setting yaal root dir.

--- configure.js
+++ configure.js
@@ -1,2 +1,4 @@
 var fs = new ActiveXObject( "Scripting.FileSystemObject" );
-eval( fs.openTextFile( fs.getParentFolderName( fs.getFile( WScript.scriptFullName ) ) + "/../yaal/configure.js", 1 ).readAll() );
+var envYaalSourcePath = WScript.createObject( "WScript.Shell" ).environment( "Process" )( "YAAL_SOURCE_PATH" );
+var yaalSourcePath = fs.getParentFolderName( fs.getFile( WScript.scriptFullName ) ) + "/../yaal";
+eval( fs.openTextFile( ( envYaalSourcePath != "" ? envYaalSourcePath : yaalSourcePath ) + "/configure.js", 1 ).readAll() );
