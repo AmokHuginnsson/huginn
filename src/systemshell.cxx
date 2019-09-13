43cc5afd7fd07b8850e350e5ff8512b11986ab2f
Marcin Konarski <marcin.konarski@codestation.org>
2019-09-13 14:35:33 +0200

    Improve shell completions.

--- src/systemshell.cxx
+++ src/systemshell.cxx
@@ -884,10 +884,10 @@ bool HSystemShell::fallback_completions( yaal::hcore::HString const& context_, t
 	M_EPILOG
 }
 
-void HSystemShell::filename_completions( tokens_t const& tokens_, yaal::hcore::HString const& context_, yaal::hcore::HString const& prefix_, FILENAME_COMPLETIONS filenameCompletions_, completions_t& completions_ ) const {
+void HSystemShell::filename_completions( tokens_t const& tokens_, yaal::hcore::HString const& prefix_, FILENAME_COMPLETIONS filenameCompletions_, completions_t& completions_ ) const {
 	M_PROLOG
 	static HString const SEPARATORS( "/\\" );
-	bool wantExec( tokens_.is_empty() || ( ( tokens_.get_size() == 1 ) && ! context_.ends_with( " " ) ) );
+	bool wantExec( tokens_.is_empty() || ( ( tokens_.get_size() == 1 ) && ! prefix_.ends_with( " " ) ) );
 	HString context( ! tokens_.is_empty() ? tokens_.back() : "" );
 	HString prefix(
 		! context.is_empty()
@@ -937,13 +937,13 @@ void HSystemShell::filename_completions( tokens_t const& tokens_, yaal::hcore::H
 	M_EPILOG
 }
 
-void HSystemShell::user_completions( yaal::tools::HHuginn::value_t const& userCompletions_, tokens_t const& tokens_, yaal::hcore::HString const& context_, yaal::hcore::HString const& prefix_, completions_t& completions_ ) const {
+void HSystemShell::user_completions( yaal::tools::HHuginn::value_t const& userCompletions_, tokens_t const& tokens_, yaal::hcore::HString const& prefix_, completions_t& completions_ ) const {
 	M_PROLOG
 	HHuginn::type_id_t t( !! userCompletions_ ? userCompletions_->type_id() : tools::huginn::type_id( HHuginn::TYPE::NONE ) );
 	if ( t == HHuginn::TYPE::TUPLE ) {
 		tools::huginn::HTuple::values_t const& data( static_cast<tools::huginn::HTuple const*>( userCompletions_.raw() )->value() );
 		for ( HHuginn::value_t const& v : data ) {
-			user_completions( v, tokens_, context_, prefix_, completions_ );
+			user_completions( v, tokens_, prefix_, completions_ );
 		}
 	} else if ( t == HHuginn::TYPE::LIST ) {
 		tools::huginn::HList::values_t const& data( static_cast<tools::huginn::HList const*>( userCompletions_.raw() )->value() );
@@ -957,11 +957,11 @@ void HSystemShell::user_completions( yaal::tools::HHuginn::value_t const& userCo
 		HString completionAction( tools::huginn::get_string( userCompletions_ ) );
 		completionAction.lower();
 		if ( ( completionAction == "directories" ) || ( completionAction == "dirs" ) ) {
-			filename_completions( tokens_, context_, prefix_, FILENAME_COMPLETIONS::DIRECTORY, completions_ );
+			filename_completions( tokens_, prefix_, FILENAME_COMPLETIONS::DIRECTORY, completions_ );
 		} else if ( completionAction == "files" ) {
-			filename_completions( tokens_, context_, prefix_, FILENAME_COMPLETIONS::FILE, completions_ );
+			filename_completions( tokens_, prefix_, FILENAME_COMPLETIONS::FILE, completions_ );
 		} else if ( completionAction == "executables" ) {
-			filename_completions( tokens_, context_, prefix_, FILENAME_COMPLETIONS::EXECUTABLE, completions_ );
+			filename_completions( tokens_, prefix_, FILENAME_COMPLETIONS::EXECUTABLE, completions_ );
 		} else if ( completionAction == "commands" ) {
 			for ( system_commands_t::value_type const& sc : _systemCommands ) {
 				if ( sc.first.starts_with( prefix_ ) ) {
@@ -1009,7 +1009,15 @@ bool HSystemShell::is_prefix( yaal::hcore::HString const& stem_ ) const {
 
 HShell::completions_t HSystemShell::do_gen_completions( yaal::hcore::HString const& context_, yaal::hcore::HString const& prefix_ ) const {
 	M_PROLOG
-	tokens_t tokens( split_quotes_tilda( context_ ) );
+	tokens_t tokens( split_chains( context_ ).back() );
+	for ( tokens_t::iterator it( tokens.begin() ); it != tokens.end(); ) {
+		if ( ( *it == SHELL_AND ) || ( *it == SHELL_OR ) || ( *it == SHELL_PIPE ) ) {
+			++ it;
+			it = tokens.erase( tokens.begin(), it );
+		} else {
+			++ it;
+		}
+	}
 	tokens.erase( remove( tokens.begin(), tokens.end(), "" ), tokens.end() );
 	bool endsWithWhitespace( ! context_.is_empty() && character_class<CHARACTER_CLASS::WHITESPACE>().has( context_.back() ) );
 	if ( endsWithWhitespace ) {
@@ -1026,12 +1034,11 @@ HShell::completions_t HSystemShell::do_gen_completions( yaal::hcore::HString con
 	}
 	completions_t completions;
 	if ( !! userCompletions && ( userCompletions->type_id() != HHuginn::TYPE::NONE ) ) {
-		user_completions( userCompletions, tokens, context_, prefix_, completions );
+		user_completions( userCompletions, tokens, prefix_, completions );
 	} else {
-		if ( ! fallback_completions( context_, tokens, completions ) ) {
+		if ( ! fallback_completions( ! tokens.is_empty() ? tokens.front() : "", tokens, completions ) ) {
 			filename_completions(
 				tokens,
-				context_,
 				prefix_,
 				FILENAME_COMPLETIONS::FILE,
 				completions
@@ -1243,7 +1250,6 @@ bool HSystemShell::do_try_command( yaal::hcore::HString const& str_ ) {
 bool HSystemShell::do_is_valid_command( yaal::hcore::HString const& str_ ) const {
 	M_PROLOG
 	chains_t chains( split_chains( str_ ) );
-	tokens_t exploded;
 	for ( tokens_t& tokens : chains ) {
 		if ( tokens.is_empty() ) {
 			continue;
--- src/systemshell.hxx
+++ src/systemshell.hxx
@@ -103,8 +103,8 @@ private:
 		EXECUTABLE
 	};
 	bool fallback_completions( yaal::hcore::HString const&, tokens_t const&, completions_t& ) const;
-	void filename_completions( tokens_t const&, yaal::hcore::HString const&, yaal::hcore::HString const&, FILENAME_COMPLETIONS, completions_t& ) const;
-	void user_completions( yaal::tools::HHuginn::value_t const&, tokens_t const&, yaal::hcore::HString const&, yaal::hcore::HString const&, completions_t& ) const;
+	void filename_completions( tokens_t const&, yaal::hcore::HString const&, FILENAME_COMPLETIONS, completions_t& ) const;
+	void user_completions( yaal::tools::HHuginn::value_t const&, tokens_t const&, yaal::hcore::HString const&, completions_t& ) const;
 	bool is_prefix( yaal::hcore::HString const& ) const;
 private:
 	virtual bool do_is_valid_command( yaal::hcore::HString const& ) const override;
