b577f9ef18743f478392347d1ab6858846473e42
Marcin Konarski <marcin.konarski@codestation.org>
2020-05-23 03:58:13 +0200

    Allow using `gdb` with development runtime of `huginn`.

--- tests/start-dev-shell.sh
+++ tests/start-dev-shell.sh
@@ -4,7 +4,11 @@ test -z "${DEV_SHELL}" && DEV_SHELL=hgnsh exec bash "${0}" "${@}"
 unset DEV_SHELL
 
 export YAAL_AUTO_SANITY=yes
+export SHELL=${HOME}/src/huginn/tests/reldeb/hgnsh
 exec -a "${0}" ${HOME}/src/huginn/build/reldeb/huginn/1exec -L ${HOME}/var/log/hgnsh.log -qs "${@}"
-LD_LIBRARY_PATH=${HOME}/usr/lib:/home/konarski/lib:${HOME}/usr/local/lib:/usr/local/lib exec -a "${0}" ${HOME}/src/huginn/build/debug/huginn/1exec -L ${HOME}/var/log/hgnsh.log -qs "${@}"
+
+export SHELL=${HOME}/src/huginn/tests/debug/hgnsh
+export LD_LIBRARY_PATH=${HOME}/usr/lib:/home/konarski/lib:${HOME}/usr/local/lib:/usr/local/lib
+exec -a "${0}" ${HOME}/src/huginn/build/debug/huginn/1exec -L ${HOME}/var/log/hgnsh.log -qs "${@}"
 
 # vim: ft=sh
