9d598588db9ab67d6bcc631afad1894f882ebeea
Marcin Konarski <marcin.konarski@codestation.org>
2020-12-17 00:45:03 +0100

    tests, samples: Improve development experience on Windows.

--- tests/start-dev.ps1
+++ tests/start-dev.ps1
@@ -5,7 +5,7 @@ function make_absolute( [string]$path ) {
 	return $path.Replace( "\", "/" )
 }
 
-Select-String -ErrorAction Ignore -Path "local.js" -Pattern "PREFIX\s=\s[`"]([^`"]+)[`"]" | ForEach-Object {
+Select-String -ErrorAction Ignore -Path "local.js" -Pattern "PREFIX\s*=\s*[`"]([^`"]+)[`"]" | ForEach-Object {
 	$prefix = make_absolute( "$($_.Matches.groups[1])" )
 }
 $origEnvPath=$env:Path
@@ -13,7 +13,7 @@ $env:Path="$prefix\bin;$env:Path"
 $env:YAAL_AUTO_SANITY=1
 $env:HUGINN_INIT="./src/init.sample"
 $env:HUGINN_INIT_SHELL="./src/init.shell.sample"
-$env:HUGINN_RC_SHELL="./src/rc.shell.sample"
+$env:HUGINN_RC_SHELL="./tests/rc.shell.win.test"
 .\build\debug\huginn\1exec -M packages $args
 $env:Path=$origEnvPath
 
