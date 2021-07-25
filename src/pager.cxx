c9b5139bc7fb7b7548cba63bd18d25f221958a14
Marcin Konarski <marcin.konarski@codestation.org>
2021-07-25 03:00:42 +0200

    pager(): Rewrite to allow `up` movement.

--- src/pager.cxx
+++ src/pager.cxx
@@ -68,33 +68,86 @@ void pager( yaal::hcore::HString const& str_ ) {
 	string::tokens_t lines( string::split( str_, '\n' ) );
 	HTerminal& term( HTerminal::get_instance() );
 	HTerminal::HSize termSize( term.size() );
-	int lineCount( 0 );
-	bool goToEnd( false );
-	bool oneLine( false );
 	HUTF8String utf8;
-	for ( HString const& line : lines ) {
-		int lineLength( line_length( line ) );
+	bool loop( true );
+	int row( 0 );
+	bool justStarted( true );
+	auto line_height = [&termSize]( yaal::hcore::HString const& line_ ) {
+		int lineLength( line_length( line_ ) );
 		int lineHeight( lineLength / termSize.columns() + ( lineLength % termSize.columns() ? 1 : 0 ) );
 		if ( lineHeight == 0 ) {
 			++ lineHeight;
 		}
-		lineCount += lineHeight;
-		if ( oneLine || ( ! goToEnd && ( lineCount >= ( termSize.lines() - 1 ) ) ) ) {
-			oneLine = false;
-			REPL_print( PROMPT );
-			code_point_t key( term.get_key() );
-			REPL_print( "%s%s%s", ERASE, SPACE, ERASE );
-			if ( key == 'q' ) {
-				break;
-			} else if ( ( key == 'G' ) || ( key == KEY_CODE::END ) ) {
-				goToEnd = true;
-			} else if ( ( key == '\r' ) || ( key == KEY_CODE::DOWN ) ) {
-				oneLine = true;
-			}
-			lineCount = lineHeight;
+		return ( lineHeight );
+	};
+
+	while ( loop ) {
+		int lineCount( 0 );
+		int rowInPage( 0 );
+		while ( ( ( row + rowInPage ) < lines.get_size() ) && ( lineCount < ( termSize.lines() - ( justStarted ? 2 : 1 ) ) ) ) {
+			HString const& line( lines[row + rowInPage] );
+			lineCount += line_height( line );
+			utf8.assign( line );
+			REPL_print( "%s\n", utf8.c_str() );
+			++ rowInPage;
 		}
-		utf8.assign( line );
-		REPL_print( "%s\n", utf8.c_str() );
+		if ( ( row + rowInPage ) == lines.get_size() ) {
+			break;
+		}
+		justStarted = false;
+		-- rowInPage;
+
+		REPL_print( PROMPT );
+		code_point_t key( term.get_key() );
+		REPL_print( "%s%s%s", ERASE, SPACE, ERASE );
+		switch ( key.get() ) {
+			case ( KEY_CODE::PAGE_DOWN ): /* fall-through */
+			case ( ' ' ): {
+				row += rowInPage;
+			} break;
+			case ( KEY_CODE::END ): /* fall-through */
+			case ( 'G' ): {
+				if ( lines.is_empty() ) {
+					break;
+				}
+				row = static_cast<int>( lines.get_size() ) - 1;
+			} /* fall-through */
+			case ( KEY_CODE::PAGE_UP ): /* fall-through */
+			case ( 'b' ): {
+				lineCount = 0;
+				while ( true ) {
+					HString const& line( lines[row] );
+					lineCount += line_height( line );
+					if ( lineCount >= ( termSize.lines() - 1 ) ) {
+						break;
+					}
+					if ( row == 0 ) {
+						break;
+					}
+					-- row;
+				}
+			} break;
+			case ( KEY_CODE::HOME ): /* fall-through */
+			case ( 'g' ): {
+				row = 0;
+			} break;
+			case ( KEY_CODE::DOWN ): /* fall-through */
+			case ( '\r' ): {
+				++ row;
+			} break;
+			case ( KEY_CODE::UP ): /* fall-through */
+			case ( '\b' ): {
+				if ( row > 0 ) {
+					-- row;
+				}
+			} break;
+			case ( 'q' ): {
+				loop = 0;
+			} break;
+		}
+		if ( row >= lines.get_size() ) {
+			loop = false;
+		}
 	}
 	return;
 	M_EPILOG
