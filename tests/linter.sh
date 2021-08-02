c2c89619fd65aa8918ed6f4d379c12062f7dcc45
Marcin Konarski <marcin.konarski@codestation.org>
2021-08-02 17:57:40 +0200

    tests: Fix linter script so it can find Huginn dependencies (path to dev packages; local module path).

--- tests/linter.sh
+++ tests/linter.sh
@@ -4,6 +4,6 @@ set -eu
 
 find packages -name '*.hgn' | sort | while read package ; do
 	assumeUsed="$(awk "/$(basename ${package})/{print \$2}" ./tests/data/assume-used.lst)"
-	( set -x && ./build/release/huginn/1exec --lint --assume-used "${assumeUsed}" "${package}" )
+	( set -x && ./build/release/huginn/1exec --module-path packages --lint --assume-used "${assumeUsed}" "${package}" )
 done
 
