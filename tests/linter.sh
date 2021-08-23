9371fba0debbb3a27fc19c23fb7602cb05db4d87
Marcin Konarski <marcin.konarski@codestation.org>
2021-08-24 01:42:55 +0200

    tests: Allow running linter using also `debug` target.

--- tests/linter.sh
+++ tests/linter.sh
@@ -2,8 +2,10 @@
 
 set -eu
 
+target="${TARGET:-debug}"
+
 find packages -name '*.hgn' | sort | while read package ; do
 	assumeUsed="$(awk "/$(basename ${package})/{print \$2}" ./tests/data/assume-used.lst)"
-	( set -x && ./build/release/huginn/1exec --module-path packages --lint --assume-used "${assumeUsed}" "${package}" )
+	( set -x && ./build/${target}/huginn/1exec --module-path packages --lint --assume-used "${assumeUsed}" "${package}" )
 done
 
